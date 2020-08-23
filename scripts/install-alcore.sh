47ba522bb7871758638c632e280df33c307a2169
Akshay Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-08-24 03:46:37 +0530

    chore(source): fix sh variable ref

--- scripts/install-alcore.sh
+++ scripts/install-alcore.sh
@@ -27,13 +27,21 @@ export LC_CTYPE="en_US.UTF-8"
 #export LC_TELEPHONE=?~@~]es_ES.UTF-8?~@~]
 #export LC_MEASUREMENT=?~@~]es_ES.UTF-8?~@~]
 #export LC_IDENTIFICATION=?~@~]es_ES.UTF-8?~@~]
-source ./config.sh
 
-git clone --single-branch --branch $OPENEDX_RELEASE_BRANCH git@github.com:foundercore/configuration.git $CONF_DIR
-git clone --single-branch --branch $OPENEDX_RELEASE_BRANCH git@github.com:foundercore/configuration.git /tmp/configuration
-cp $CONF_DIR/util/install/* $ROOT_DIR/
+# 1. Set the OPENEDX_RELEASE variable:
+#export OPENEDX_RELEASE=open-release/ginkgo.2
+# Note: sometimes there are important bug fixes in master that are not included in the named releases.
+#       to date i've always had the best luck with master.
+export OPENEDX_RELEASE=open-release/juniper.master
+export ROOT_DIR=/home/ubuntu/.alcore
+export NATIVE_DIR=$ROOT_DIR/native
+export CONF_DIR=/var/tmp/configuration
 
-cd $ROOT_DIR
+git clone --single-branch --branch $(OPENEDX_RELEASE_BRANCH) git@github.com:foundercore/configuration.git $(CONF_DIR)
+git clone --single-branch --branch $(OPENEDX_RELEASE_BRANCH) git@github.com:foundercore/configuration.git /tmp/configuration
+cp $(CONF_DIR)/util/install/* $(ROOT_DIR)/
+
+cd $(ROOT_DIR)
 
 sudo -E bash ansible-bootstrap.sh
 . /edx/app/edx_ansible/venvs/edx_ansible/bin/activate
