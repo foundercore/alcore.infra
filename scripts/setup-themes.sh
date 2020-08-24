99058116d8156bb6ab3b95267041ce172a58148b
Akshay Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-08-25 04:17:29 +0530

    chore(source): update setup.theme state

--- scripts/setup-themes.sh
+++ scripts/setup-themes.sh
@@ -1,8 +1,6 @@
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
-sudo rm -rf /opt/bitnami/apps/edx/var/themes/*
-cd /opt/bitnami/apps/edx/var/themes
-
-sudo git clone https://github.com/foundercore/alcore.themes.git .
-sudo chown -R daemon.daemon .
+sudo rm -rf /opt/bitnami/apps/edx/var/themes
+sudo git clone https://github.com/foundercore/alcore.themes.git /opt/bitnami/apps/edx/var/themes
+sudo chown -R daemon.daemon /opt/bitnami/apps/edx/var/themes
