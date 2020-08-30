ea1e7ae61e5decff54db82c545bd06fab6e79f61
Akshay Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-08-30 14:51:24 +0530

    chore(source): ssh connect fix

--- local.mk
+++ local.mk
@@ -2,13 +2,13 @@
 fix.ssh:
 	sudo chmod 600 -R ./tmp/*.pem
 
-clean.demo:
+clean.demo: fix.ssh
 	ssh-keygen -f "/home/$(USER)/.ssh/known_hosts" -R "ec2-54-145-33-149.compute-1.amazonaws.com"
 
 ssh.demo: clean.demo
 	ssh -i ./tmp/alcore-us-east-1-demo.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
 
-clean.learn:
+clean.learn: fix.ssh
 	ssh-keygen -f "/home/$(USER)/.ssh/known_hosts" -R "ec2-35-174-190-84.compute-1.amazonaws.com"
 
 ssh.learn: clean.learn
@@ -16,3 +16,6 @@ ssh.learn: clean.learn
 
 tunnel.demo:
 	ssh -N -L 8888:127.0.0.1:80 -i ./tmp/alcore-us-east-1-demo.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
+
+tunnel.learn:
+	ssh -N -L 8888:127.0.0.1:80 -i ./tmp/alcore-us-east-1-learn.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
