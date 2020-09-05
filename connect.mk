895bc6f9841d9e68402f1fbcb691eb198833bf68
Akshay Kr Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-09-05 21:02:49 +0530

    feat(make): add ssh for dev instance (#1)

--- connect.mk
+++ connect.mk
@@ -14,6 +14,9 @@ clean.learn:
 ssh.learn:
 	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
 
+ssh.dev:
+	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-160-60-161.compute-1.amazonaws.com
+
 tunnel.demo:
 	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
 
