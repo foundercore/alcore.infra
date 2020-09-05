ef800335e8ffbaade6cc7203e32e2968aa737882
Akshay Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-09-05 20:22:19 +0530

    feat(base): release(ironwood.master)

--- connect.mk
+++ connect.mk
@@ -2,20 +2,23 @@
 fix.ssh:
 	sudo chmod 600 -R ./local/*.pem
 
-clean.demo: fix.ssh
+clean.demo:
 	ssh-keygen -f "/home/$(USER)/.ssh/known_hosts" -R "ec2-54-145-33-149.compute-1.amazonaws.com"
 
-ssh.demo: clean.demo
-	ssh -i ./local/alcore-us-east-1-demo.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
+ssh.demo:
+	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
 
-clean.learn: fix.ssh
+clean.learn:
 	ssh-keygen -f "/home/$(USER)/.ssh/known_hosts" -R "ec2-35-174-190-84.compute-1.amazonaws.com"
 
-ssh.learn: clean.learn
-	ssh -i ./local/alcore-us-east-1-learn.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
+ssh.learn:
+	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
 
 tunnel.demo:
-	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-demo.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
+	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
 
 tunnel.learn:
-	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-learn.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
+	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
+
+tunnel.dev:
+	ssh -N -L 8888:127.0.0.1:80 -i ./tmp/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-160-60-161.compute-1.amazonaws.com
