56ff69b33ea181eab8d429600d3d1407cf6addee
Akshay Kumar Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-09-19 15:09:07 +0530

    fix(update.engine): make copy recursively

--- scripts/cloud/setup-engine.sh
+++ scripts/cloud/setup-engine.sh
@@ -3,6 +3,6 @@ sudo rm -rf ~/alcore.edx
 git clone git@github.com:foundercore/alcore.edx.git ~/alcore.edx
 
 sudo mkdir -p /opt/bitnami/apps/edx/edx-platform
-sudo cp ~/alcore.edx /opt/bitnami/apps/edx/edx-platform
+sudo cp -r ~/alcore.edx /opt/bitnami/apps/edx/edx-platform
 
 sudo chown root.root /opt/bitnami/apps/edx/edx-platform
