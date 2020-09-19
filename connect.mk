5a8a9722afdad4ef654f0afad2a807e5c5786aa0
Akshay Kumar Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-09-19 14:55:35 +0530

    fix(update.engine): try clone w/o sudo

--- connect.mk
+++ connect.mk
@@ -8,23 +8,20 @@ clean.demo:
 ssh.demo:
 	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
 
-clean.learn:
-	ssh-keygen -f "/home/$(USER)/.ssh/known_hosts" -R "ec2-35-174-190-84.compute-1.amazonaws.com"
-
-clean.dev:
-	ssh-keygen -f "/home/$(USER)/.ssh/known_hosts" -R "ec2-54-160-60-161.compute-1.amazonaws.com"
-
-ssh.learn:
-	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
+clean.endeavor:
+	ssh-keygen -f "/Users/$(USER)/.ssh/known_hosts" -R "ec2-35-174-190-84.compute-1.amazonaws.com"
 
 ssh.dev:
 	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-160-60-161.compute-1.amazonaws.com
 
+ssh.endeavor:
+	ssh -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
+
 tunnel.demo:
 	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-145-33-149.compute-1.amazonaws.com
 
-tunnel.learn:
+tunnel.endeavor:
 	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-35-174-190-84.compute-1.amazonaws.com
 
 tunnel.dev:
-	ssh -N -L 8888:127.0.0.1:80 -i ./local/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-160-60-161.compute-1.amazonaws.com
+	ssh -N -L 8888:127.0.0.1:80 -i ./tmp/alcore-us-east-1-bitnami-key.pem bitnami@ec2-54-160-60-161.compute-1.amazonaws.com
--- scripts/cloud/setup-engine.sh
+++ scripts/cloud/setup-engine.sh
@@ -1,6 +1,1 @@
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
-sudo rm -rf /opt/bitnami/apps/edx/edx-platform
-sudo git clone git@github.com:foundercore/alcore.edx.git /opt/bitnami/apps/edx/edx-platform
-sudo chown -R daemon.daemon /opt/bitnami/apps/edx/edx-platform
+git clone git@github.com:foundercore/alcore.edx.git ~/alcore.edx
