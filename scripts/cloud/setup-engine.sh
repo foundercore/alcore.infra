8a8dc4ca1bd58b727e8f1d60140e2126d4ae1569
Akshay Kumar Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-09-19 15:05:19 +0530

    fix(update.engine): refactor update.engine process

--- scripts/cloud/setup-engine.sh
+++ scripts/cloud/setup-engine.sh
@@ -1,1 +1,8 @@
+
+sudo rm -rf ~/alcore.edx
 git clone git@github.com:foundercore/alcore.edx.git ~/alcore.edx
+
+mkdir -p /opt/bitnami/apps/edx/edx-platform
+sudo cp ~/alcore.edx /opt/bitnami/apps/edx/edx-platform
+
+sudo chmod root.root /opt/bitnami/apps/edx/edx-platform
