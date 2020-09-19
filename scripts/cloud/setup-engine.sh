9bf2974ee300a369ecd613af7d21f823cd1322f7
Akshay Kumar Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-09-19 15:07:14 +0530

    fix(update.engine): fix chown

--- scripts/cloud/setup-engine.sh
+++ scripts/cloud/setup-engine.sh
@@ -2,7 +2,7 @@
 sudo rm -rf ~/alcore.edx
 git clone git@github.com:foundercore/alcore.edx.git ~/alcore.edx
 
-mkdir -p /opt/bitnami/apps/edx/edx-platform
+sudo mkdir -p /opt/bitnami/apps/edx/edx-platform
 sudo cp ~/alcore.edx /opt/bitnami/apps/edx/edx-platform
 
-sudo chmod root.root /opt/bitnami/apps/edx/edx-platform
+sudo chown root.root /opt/bitnami/apps/edx/edx-platform
