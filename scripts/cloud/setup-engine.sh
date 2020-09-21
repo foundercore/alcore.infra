6d49e25e21159cf75f754d9d3b0f0be94d0d73a7
ahmer09 <ahmeriq09@hotmail.com>
2020-09-21 03:47:02 +0000

    Update setup-engine.sh

--- scripts/cloud/setup-engine.sh
+++ scripts/cloud/setup-engine.sh
@@ -1,8 +1,9 @@
 
 sudo rm -rf ~/alcore.edx
+sudo rm -rf /opt/bitnami/apps/edx/edx-platform
 git clone git@github.com:foundercore/alcore.edx.git ~/alcore.edx
 
 sudo mkdir -p /opt/bitnami/apps/edx/edx-platform
-sudo cp -r ~/alcore.edx /opt/bitnami/apps/edx/edx-platform
+sudo rsync -avz ~/alcore.edx/* /opt/bitnami/apps/edx/edx-platform
 
-sudo chown root.root /opt/bitnami/apps/edx/edx-platform
+sudo chown -R root.root /opt/bitnami/apps/edx/edx-platform
